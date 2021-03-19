0e0cf5e59ace2e6d37c8486e421573547cc7e6f2
HelloArthur <67996100+arthurCheng2Playitloud@users.noreply.github.com>
2021-03-19 23:03:43 +0800

    init Block.java

---
+++ src/com/company/Block.java
@@ -0,0 +1,11 @@
+package com.company;
+import java.util.*;
+import java.sql.Timestamp;
+
+public class Block {
+    public int index;
+    public int timestamp;
+    public String hash;
+    public String previousHash;
+    public String data;
+}
