9605581d39f3b97bd19bf71d16f807f323644bee
HelloArthur <67996100+arthurCheng2Playitloud@users.noreply.github.com>
2021-04-03 21:06:45 +0800

    basic block prototype init

--- src/com/company/Block.java
+++ src/com/company/Block.java
@@ -1,11 +1,42 @@
 package com.company;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.sql.Timestamp;
+import java.security.MessageDigest;
 
 public class Block {
     public int index;
-    public int timestamp;
+    public double timestamp;
     public String hash;
     public String previousHash;
     public String data;
+
+    public Block() {}
+
+    public Block(int index, double timestamp, String previousHash, String data) {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.hash = hash(this.index, this.timestamp, this.previousHash, this.data);
+    }
+
+    public String hash(int index, double timestamp, String previousHash, String data) {
+        try{
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            String blockInformation = (index + previousHash + timestamp  + data);
+            byte[] hash = digest.digest(blockInformation.getBytes(StandardCharsets.UTF_8));
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < hash.length; i++) {
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        }catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,23 @@
 package com.company;
 
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World!");
     }
+
+    public Block generateNextBlock(String blockData) {
+        Block previousBlock = getLatestBlock();
+        int nextIndex = previousBlock.index + 1;
+        double nextTimestamp = new Date().getTime() / 1000;
+        return new Block(nextIndex, nextTimestamp, previousBlock.hash, blockData);
+    }
+
+    public Block getLatestBlock() {
+        return new Block();
+    }
+
 }
